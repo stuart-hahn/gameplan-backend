a20172636158249cb09b07e97f435ee15f649b47
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-01-14 15:41:04 -0500

    update controllers with basic actions, fix routes

--- app/controllers/api/v1/formations_controller.rb
+++ app/controllers/api/v1/formations_controller.rb
@@ -1,19 +1,51 @@
 class Api::V1::FormationsController < ApplicationController
+    before_action :set_formation, only: [:show, :update, :destroy]
+
+    # GET /formations
     def index
-        formations = Formation.all
-        options = {
-            include: [:plays]
-        }
-        render json: FormationSerializer.new(formations, options)
+      @formations = Formation.all
+  
+      render json: FormationSerializer.new(@formations)
     end
-
+  
+    # GET /formations/1
+    def show
+      render json: FormationSerializer.new(@formation)
+    end
+  
+    # POST /formations
     def create
-        formation = Formation.create(formation_params)
+      @formation = Formation.new(formation_params)
+  
+      if @formation.save
+        render json: FormationSerializer(@formation), status: :created, location: @formation
+      else
+        render json: @formation.errors, status: :unprocessable_entity
+      end
     end
-
+  
+    # PATCH/PUT /formations/1
+    def update
+      if @formation.update(formation_params)
+        render json: FormationSerializer.new(@formation)
+      else
+        render json: @formation.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /formations/1
+    def destroy
+      @formation.destroy
+    end
+  
     private
-
-    def formation_params
+      # Use callbacks to share common setup or constraints between actions.
+      def set_formation
+        @formation = Formation.find_by(id: params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def formation_params
         params.require(:formation).permit(:name, :playbook)
-    end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :formations do
-        resources :plays
+        resources :plays, shallow: true
       end
+      resources :plays
     end
   end
   
