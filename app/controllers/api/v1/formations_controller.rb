d66f2f3619aa7a65452c499a222b25c8b2d74d94
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-01-14 14:28:08 -0500

    namespace routes

--- app/controllers/api/v1/formations_controller.rb
+++ app/controllers/api/v1/formations_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::FormationsController < ApplicationController
+    def index
+        formations = Formation.all
+        render json: formations
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :plays
-  resources :formations
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :formations do
+        resources :plays
+      end
+    end
+  end
+  
 end
